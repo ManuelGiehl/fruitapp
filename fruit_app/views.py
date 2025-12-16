7a39586a25bf0b9a160e8426313e37a964e7a629
Manuel <manuzcore1@gmail.com>
2025-12-16 09:22:22 +0100

    feat: add send_fruits view with 5 objects also added a JsonResponse

--- fruit_app/views.py
+++ fruit_app/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def send_fruits(request):
+    fruits = [
+        {
+            'name': 'Apfel',
+            'gewicht': 150,
+            'farbe': 'Rot'
+        },
+        {
+            'name': 'Banane',
+            'gewicht': 120,
+            'farbe': 'Gelb'
+        },
+        {
+            'name': 'Orange',
+            'gewicht': 200,
+            'farbe': 'Orange'
+        },
+        {
+            'name': 'Erdbeere',
+            'gewicht': 20,
+            'farbe': 'Rot'
+        },
+        {
+            'name': 'Traube',
+            'gewicht': 5,
+            'farbe': 'Lila'
+        }
+    ]
+    
+    return JsonResponse({'fruits': fruits})
